8fa111dba82d139cc9a6d9691e1fd869192b11c4
Eugen Hulub <eugenhulub@gmail.com>
2025-03-26 02:36:16 -0700

    sim lidar

--- simulator/recompile.sh
+++ simulator/recompile.sh
@@ -9,4 +9,3 @@ catkin_make
 curr_wd=$(pwd) #get the current working directory
 echo export GAZEBO_MODEL_PATH=\""$curr_wd/src/models_pkg:\$GAZEBO_MODEL_PATH"\" >> devel/setup.bash
 echo export ROS_PACKAGE_PATH=\""$curr_wd/src:\$ROS_PACKAGE_PATH"\" >> devel/setup.bash
-
